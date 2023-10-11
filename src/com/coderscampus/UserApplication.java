df856ccf883825a08dea0d73914b6e20b6210343
Charles Knowlton <clknowlton@icloud.com>
2023-10-10 19:30:15 -0500

    WIP

--- src/com/coderscampus/UserApplication.java
+++ src/com/coderscampus/UserApplication.java
@@ -1,11 +1,29 @@
 package com.coderscampus;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class UserApplication {
 
 	public static void main(String[] args) {
 		
-		
-		
+		BufferedReader fileReader = null;
+		try {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+			String line = "";
+			while ((line = fileReader.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Oops, the file wasn't found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Oops, you encountered an I/O Exception");
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/com/coderscampus/UserService.java
@@ -0,0 +1,17 @@
+package com.coderscampus;
+
+public class UserService {
+
+	public User createUser(String[] stringInput) {
+		User user = new User();
+		user.setUsername(stringInput[0]);
+		user.setPassword(stringInput[1]);
+		user.setName(stringInput[2]);
+		return user;
+	}
+
+
+
+
+
+}
