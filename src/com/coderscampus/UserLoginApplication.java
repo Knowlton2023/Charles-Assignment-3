c9ecf5ef1d544b5c44a1869671c466a18c340ba9
Charles Knowlton <clknowlton@icloud.com>
2023-10-16 03:32:31 -0500

    WIP

--- src/com/coderscampus/UserLoginApplication.java
+++ src/com/coderscampus/UserLoginApplication.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class UserApplication {
+public class UserLoginApplication {
 
 	public static void main(String[] args) {
 		
@@ -45,23 +45,48 @@ public class UserApplication {
 				e.printStackTrace();
 			}
 		}
+
+
+
+		int guessCounter = 0;  // no guess attempt made yet
+		while (guessCounter < 5) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Type in your Username:");
+			String usernameInput = scanner.nextLine();
+			System.out.println("Type in your Password:");
+			String passwordInput = scanner.nextLine();
+
+			for (User user : users) {
+				System.out.println(user.getUsername());
+				System.out.println(user.getPassword());
+				System.out.println(user.getName());
+
+				// valid guess range, check correctness
+				if (usernameInput == user.getUsername() && passwordInput == user.getPassword()) {
+					System.out.println("Welcome " + user.getName());
+					break;
+				} else if (guessCounter == 5) {
+					System.out.println("Too many failed login attempts, you are now locked out.");
+					
+				} else {
+					System.out.println("Invalid login, please try again.");
+					break;
+				}
+				scanner.close();
+			}
+			guessCounter++;
+		
+		}
+		
 		// Testing ONLY
 //		for (User user : users) {
 //			System.out.println("Username: " + user.getUsername());
 //			System.out.println("Password: " + user.getPassword());
 //			System.out.println("Name: " + user.getName());
 //		}
-
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Type in your Username:");
-		String usernameInput = scanner.nextLine();
-		System.out.println("Type in your Password:");
-		String passwordInput = scanner.nextLine();
-
-		System.out.println("Username: " + usernameInput + " Password: " + passwordInput);
+	
 		
-		System.out.println("Closing scanner");
-		scanner.close();
+		
 		
 	
 	
