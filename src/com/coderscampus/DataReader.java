d2dbbd482b90626493863e4413c298348ec3a325
Charles Knowlton <clknowlton@icloud.com>
2023-10-18 15:32:02 -0500

    WIP

--- src/com/coderscampus/DataReader.java
+++ src/com/coderscampus/DataReader.java
@@ -7,37 +7,36 @@ import java.io.IOException;
 
 public class DataReader {
 
-	BufferedReader fileReader = null;
-	String line = "";
-	String[] inputs = null;
-	UserService userService = new UserService();
-	User[] users = new User[4];
-	try {
-		fileReader = new BufferedReader(new FileReader("data.txt"));
-		int i = 0;
-		while ((line = fileReader.readLine()) != null) {
-			// This is where I will read my data into an Array 
-			// so that I can compare it later
-			inputs = line.split(",");
-			users[i] = userService.createUser(inputs);
-			i++;
-		}
-	} catch (FileNotFoundException e) {
-		System.out.println("Oops, the file wasn't found");
-		e.printStackTrace();
-	} catch (IOException e) {
-		System.out.println("Oops, you encountered an I/O Exception");
-		e.printStackTrace();
-	} finally {
+	public User[] readData() {
+	
+		User[] users = new User[4];
+		BufferedReader fileReader = null;
+		String[] inputs = null;
+		UserService userService = new UserService();
+
+		String line = "";
 		try {
-			System.out.println("Closing file reader");
-			fileReader.close();
-		} catch (IOException e) {
+			fileReader = new BufferedReader(new FileReader("data.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Oops, file not found exception");
 			e.printStackTrace();
 		}
+		int i = 0;
+		try {
+			while ((line = fileReader.readLine()) != null) {
+				// This is where I will read my data into an Array 
+				// so that I can compare it later
+				inputs = line.split(",");
+				users[i] = userService.createUser(inputs);
+				i++;
+			}
+		} catch (IOException e) {
+			System.out.println("Oops, file I/O exception");
+			e.printStackTrace();
+		}
+	
+	return users;
+	
 	}
-
-
-
 
 }
